4e7367713a12eece410f69d671ef987fe7be45c7
Jake Brukhman <jbrukh@gmail.com>
2011-05-18 02:35:11 +0000

    Working

--- src/craigslist.py
+++ src/craigslist.py
@@ -9,78 +9,51 @@ from BeautifulSoup import BeautifulStoneSoup
 import urllib2
 import re
 
-categories = { 	'aap': 'all apartments',
-				'nfa': 'all no fee apts',
-				'fee': 'apts broker fee',
-				'nfb': 'apts broker no fee',
-				'abo': 'apts by owner',
-				'aiv': 'apts registration fee',
-				'hou': 'apts wanted',
-				'swp': 'housing swap',
-				'hsw': 'housing wanted',
-				'off': 'office & commercial',
-				'prk': 'parking and storage',
-				'reb': 'real estate by broker',
-				'reo': 'real estate by owner',
-				'rea': 'real estate for sale',
-				'rew': 'real estate wanted',
-				'roo': 'rooms and shares',
-				'sha': 'rooms wanted',
-				'sbw': 'sublet/temp wanted',
-				'sub': 'sublets & temporary',
-				'vac': 'vacation rentals'
-			}
+categories = {  'aap': 'all apartments',
+                'nfa': 'all no fee apts',
+                'fee': 'apts broker fee',
+                'nfb': 'apts broker no fee',
+                'abo': 'apts by owner',
+                'aiv': 'apts registration fee',
+                'hou': 'apts wanted',
+                'swp': 'housing swap',
+                'hsw': 'housing wanted',
+                'off': 'office & commercial',
+                'prk': 'parking and storage',
+                'reb': 'real estate by broker',
+                'reo': 'real estate by owner',
+                'rea': 'real estate for sale',
+                'rew': 'real estate wanted',
+                'roo': 'rooms and shares',
+                'sha': 'rooms wanted',
+                'sbw': 'sublet/temp wanted',
+                'sub': 'sublets & temporary',
+                'vac': 'vacation rentals'
+            }
 
 searchType = {
-				'T':   'titles only',
-				'A':   'entire post'
-			 }
-
-def listings(url, query='', srchType='A', bedrooms='', minAsk='1', maxAsk='', catAbb='aap', s=0):
-	"""
-	Scrapes the craigslist apartment search page and returns the listings from
-	that page.  Listings are composed of date, title, and url.
-
-        url			the search url, usually something like the following whence
-					parameters are attached.
-				
-						http://newyork.craigslist.org/search/aap/brk
-
-		query		the searh string
-		srchType	either 'T' or 'A', as above
-		minAsk		number, minimum asking price
-		maxAsk		number, maximum asking price
-		bedrooms	number, of bedrooms
-		catAbb		as categories map above
-		s			page offset, starts at the s-th listing
-	
-	This function may throw various kinds of Errors:
-
-		* if it cannot open the url
-		* if it cannot parse the data received
-
-	"""
-
-	# prepare the url
-	full_url = "%s?format=rss&query=%s&catAbb=%s&srchType=%s&minAsk=%s&maxAsk=%s&bedrooms=%s&s=%s" %\
-				(url.rstrip('/'), '+'.join(query.split(' ')), catAbb, srchType, minAsk, maxAsk, bedrooms, s)
-	fetch(full_url)
+                'T':   'titles only',
+                'A':   'entire post'
+             }
 
 def fetch(full_url):
-	print full_url
-	page = urllib2.urlopen(full_url)
-	# massage the pesky Craigslist CDATA tags so that entities are processed; TODO -- find out
-	# if it would be better to sanitize entities separately.
-	cdataMassage = [(re.compile('<!\[CDATA\[|]]>'), lambda match: '')]
-	soup = BeautifulStoneSoup(page, markupMassage=cdataMassage,
-				convertEntities=BeautifulStoneSoup.ALL_ENTITIES)
-	for item in soup('item'):
-		yield {
-    	        'date':  item('dc:date')[0].string,
-        	    'title': item.title.string,
-           	 	'link':  item.link.string
+    if not re.search('format=rss', full_url):
+        full_url += '&format=rss'
+    page = urllib2.urlopen(full_url)
+    
+    # massage the pesky Craigslist CDATA tags so that entities are processed; TODO -- find out
+    # if it would be better to sanitize entities separately.
+    cdataMassage = [(re.compile('<!\[CDATA\[|]]>'), lambda match: '')]
+    soup = BeautifulStoneSoup(page, markupMassage=cdataMassage,
+                convertEntities=BeautifulStoneSoup.ALL_ENTITIES)
+    for item in soup('item'):
+        yield {
+                'date':  item('dc:date')[0].string,
+                'title': item.title.string,
+                'link':  item.link.string
               }
 
 def fetch_all(queries):
-	for query in queries:
-		yield fetch(full_url)
+    for query in queries:
+        for listing in fetch(query):
+               yield listing
--- src/updater.py
+++ src/updater.py
@@ -12,50 +12,47 @@ def load_cache():
     # check whether the file exists
     if not os.path.exists(CACHE_FILE):
         save_cache(set())
-    file = open(CACHE_FILE,'r')
-    urls = pickle.load(file)
-    file.close()
+    fp = open(CACHE_FILE,'r')
+    urls = pickle.load(fp)
+    fp.close()
     return urls
 
 def save_cache( urls ):
     """Cache an object."""
-    file = open(CACHE_FILE,'w')
-    pickle.dump(urls, file)
-    file.close()
+    fp = open(CACHE_FILE,'w')
+    pickle.dump(urls, fp)
+    fp.close()
 
 def main(queries):
     while True:
+        print "Updating..."
         for query in queries:
-            try:
-                listings = update(queries)
-            except Exception, err:
-                print "Error:", err
-		sleep_time = random.randint(60,60*5)
-		print "Sleeping %d seconds..." % sleep_time
-		time.sleep(sleep_time)
+            listings = update(queries)
+            for listing in listings:
+                print listing 
+        time.sleep(10)
 
 def update(queries):
-	"""
-	Given the particular craigslist arguments in kwargs, retrieve
-	new listings that haven't yet been cached and return them. 
-	"""
-	visited = load_cache()
-	new_listings = []
-	listings = craigslist.fetch_all(queries)
-	for listing in listings:
-		print link
-		link = listing['link']
-		if link not in visited:
-			new_listings.append(listing)
-			visited.add(link)
-	save_cache(visited)	
-	return new_listings
+    """
+    Given the particular craigslist arguments in kwargs, retrieve
+    new listings that haven't yet been cached and return them.
+    """
+    visited = load_cache()
+    new_listings = []
+    listings = craigslist.fetch_all(queries)
+    for listing in listings:
+        link = listing['link']
+        if link not in visited:
+            new_listings.append(listing)
+            visited.add(link)
+    save_cache(visited)
+    return new_listings
 
 if __name__ == '__main__':
-	parser = optparse.OptionParser()
-	_,args = parser.parse_args()
+    parser = optparse.OptionParser()
+    _,args = parser.parse_args()
 
-	try:
-		main(args)
-	except KeyboardInterrupt:
-		print "Goodbye!"
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print "Goodbye!"
